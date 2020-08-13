70ea1913d926a45cdca6bdaf73bf63c3ce76b0c0
AlexPullen91 <alex_pullen03@hotmail.com>
2020-08-13 12:32:30 +0000

    Made connection to database created first template and updated requirements.txt

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/get_reviews')
+def get_reviews():
+    return render_template("reviews.html", reviews=mongo.db.reviews.find())
 
 
 if __name__ == '__main__':
