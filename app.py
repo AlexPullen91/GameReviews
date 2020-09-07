7ebe113a5da296a7a2aceb2034d817a776285c39
AlexPullen91 <alex_pullen03@hotmail.com>
2020-09-07 18:27:20 +0000

    replaced thumb in cover url with big cover to retrieve bigger image

--- app.py
+++ app.py
@@ -32,17 +32,19 @@ MONGO = PyMongo(APP)
 @APP.route('/search', methods=['POST', 'GET'])
 def search_game():
     if request.method == 'POST':
-        url = 'https://api-v3.igdb.com/games'
+        games_url = 'https://api-v3.igdb.com/games'
         headers = {'user-key': API_KEY}
         gameName = request.form.get('search')
-        params = {
-            'fields': 'name, genres.name, platforms.name, release_dates.human',
+        games_params = {
+            'fields': 'name, genres.name, platforms.name, release_dates.human, cover, cover.url',
             'search': f"{gameName}",
             'limit': 1
                 }
 
-        r = requests.post(url, headers=headers, params=params)
+        r = requests.post(games_url, headers=headers, params=games_params)
 
+        cover = r.json()[0]['cover']['url']
+        coverBig = cover.replace('thumb', 'cover_big')
         gameTitle = r.json()[0]['name']
         genres = r.json()[0]['genres']
         genreNames = []
@@ -60,7 +62,7 @@ def search_game():
             release_dates.append((human['human']))
 
         if r:
-            print(gameTitle, genreNames, platformNames, release_dates)
+            print(gameTitle, genreNames, platformNames, release_dates, coverBig)
             return render_template('pages/search.html')
         else:
             print('error')
